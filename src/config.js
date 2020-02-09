e109df7424c3a3b59211b8cae237e24a4b44120c
Victoria Botelho Martins <unfoldsd@gmail.com>
2020-02-09 11:11:07 -0300

    inicial

---
+++ src/config.js
@@ -0,0 +1,21 @@
+const firebase = require('firebase')
+const rebase = require('re-base')
+
+
+const config = {
+    apiKey: "AIzaSyCRZBRZ6E4ro-eHo1FwlXFi9iz5FIpyg0Q",
+    authDomain: "jardim-universitario.firebaseapp.com",
+    databaseURL: "https://jardim-universitario.firebaseio.com",
+    projectId: "jardim-universitario",
+    storageBucket: "jardim-universitario.appspot.com",
+    messagingSenderId: "24241088193",
+    appId: "1:24241088193:web:9d3e0c14777a718d13d640",
+    measurementId: "G-2RVMJHZMY9"
+};
+
+//   firebase.analytics();
+
+const app = firebase.initializeApp(config);
+const firebaseConfig = rebase.createClass(app.database())
+
+export default firebaseConfig
