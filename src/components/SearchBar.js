b9fb63fe1673bc228eec8beb4659284c81ba23e4
Victoria Botelho Martins <unfoldsd@gmail.com>
2020-10-29 01:45:35 -0300

    Fix: searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -17,14 +17,14 @@ function SearchBar() {
             <InputGroup className={rightFocus ? "input-group-focus" : ""} style={{ height: "20px" }}>
                 <Input
                     style={{ backgroundColor: "#fff" }}
-                    placeholder="Buscar planta (nome popular)"
+                    placeholder="Buscar por nome popular"
                     type="text"
                     value={word}
                     onFocus={() => setRightFocus(true)}
                     onBlur={() => setRightFocus(false)}
                     onChange={e => setWord(e.target.value)}
                 />
-                <InputGroupAddon addonType="append" style={{ height: "38px" }}>
+                <InputGroupAddon addonType="append" style={{ maxHeight: "38px" }}>
                     <InputGroupText>
                         <a href={`/pesquisa/${word}`}>
                             < i style={{ margin: "3px", color: "black", height: '20px' }} className="fas fa-search"></i>
